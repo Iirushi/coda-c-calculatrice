5ba9b380c72b62772a3cf5cebe7876cdb3cd3ba7
Iirushi <maxime41.sobczak@gmail.com>
2024-09-26 15:15:18 +0200

    etape 3 : recupérer les infos

--- main.c
+++ main.c
@@ -2,9 +2,25 @@
 #include <stdio.h>
 
 int main() {
-
+    
     int nombre1, nombre2;
     char operateur;
+
+  
+    printf("Bonjour !\n");
+
     
+    printf("Veuillez choisir un opérateur parmi la liste suivante (+, -, *, /, %%): ");
+    scanf(" %c", &operateur);
+
+   
+    printf("Veuillez entrer un premier nombre: ");
+    scanf("%d", &nombre1);
+
+   
+    printf("Veuillez entrer un second nombre: ");
+    scanf("%d", &nombre2);
+
+   
     return 0;
 }
