17f933526eb3e190f49850031b3cd153a0cdb7e3
Iirushi <maxime41.sobczak@gmail.com>
2024-09-26 15:35:41 +0200

    etape 4 : créer les conditions

--- main.c
+++ main.c
@@ -6,21 +6,35 @@ int main() {
     int nombre1, nombre2;
     char operateur;
 
-  
+
     printf("Bonjour !\n");
 
-    
     printf("Veuillez choisir un opérateur parmi la liste suivante (+, -, *, /, %%): ");
     scanf(" %c", &operateur);
 
-   
+    
     printf("Veuillez entrer un premier nombre: ");
     scanf("%d", &nombre1);
 
-   
+    
     printf("Veuillez entrer un second nombre: ");
     scanf("%d", &nombre2);
 
-   
+    
+    if (operateur == '+') {
+        printf("addition\n");
+    } else if (operateur == '-') {
+        printf("soustraction\n");
+    } else if (operateur == '*') {
+        printf("multiplication\n");
+    } else if (operateur == '/') {
+        printf("division\n");
+    } else if (operateur == '%') {
+        printf("modulo\n");
+    } else {
+        printf("L'opérateur n'est pas reconnu.\n");
+    }
+
+ 
     return 0;
 }
